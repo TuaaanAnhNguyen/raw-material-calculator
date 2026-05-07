a8f51ccf2a6b6bc517384e4f6eaee3d24b300632
TuaaanAnhNguyen <ntuananh2503@gmail.com>
2026-05-07 21:37:47 +0700

    Fix issue with the page not loading due to falcon image thingmaginy

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import AddPage from "./AddPage.tsx";
 
 const superDuperSecretAddPageUrl = import.meta.env.VITE_ADD_PAGE_URL;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/raw-material-calculator">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path={`/${superDuperSecretAddPageUrl}`} element={<AddPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
